4493879df41586dffde3386032499e49c1807d48
Chris Giersch <chgiersch@gmail.com>
2015-01-06 15:38:20 -0800

    Initial Commit

---
+++ WordPlay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WordPlay
+//
+//  Created by Chris Giersch on 1/6/15.
+//  Copyright (c) 2015 ChrisGiersch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WordPlay/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WordPlay
+//
+//  Created by Chris Giersch on 1/6/15.
+//  Copyright (c) 2015 ChrisGiersch. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
